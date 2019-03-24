9642c44c9d19f29e3748cb61adb8bbbf18e61fa1
Erik Larson <elarsonmco@zoho.com>
2019-03-24 16:43:55 -0400

    Notes

---
+++ test.js
@@ -0,0 +1,74 @@
+// declare a variable called state and set it equal to your location
+// declate another variable called myLocation and set it equal to state
+
+
+// declare a var called num and set it equal to your age
+
+
+var state = 'Phoenix';
+
+var myLocation = state;
+
+state = 'anotherstate';
+
+
+var num = 29;
+
+var bool = true;
+bool = !bool;
+
+bool = 41;
+
+var age = 91;
+
+// check to see if age is EQUAL TO 87
+
+if (age === 87) {
+
+}
+
+var city = 'pittsburgh';
+// write an if statemnt to check if city is EQUAL TO orlando
+// finish with an else clause
+
+if (city === 'Orlando') {
+    console.log('Perfect');
+} else if (city === 'Pittsburgh') {
+    console.log ('not important');
+} else {
+    console.log('S.O.L');
+}
+
+/**
+ * && (AND)
+ * || (OR)
+ * !  (NOT)
+ */
+
+if (city === 'Orlando') {
+    // true
+} else {
+    // false
+}
+
+/**
+ * AND
+ *           &&
+ * condition AND condition - both sides must be true
+ *
+ * OR
+ *           ||
+ * condition OR condition - one side must be true
+ *
+ * NOT
+ *          !
+ * opposite of value
+ */
+/** truth tables */
+if ((true && false) || !true) {
+    // will this run?
+}
+
+// git add .
+// git commit -m 'commit message'
+// git push
